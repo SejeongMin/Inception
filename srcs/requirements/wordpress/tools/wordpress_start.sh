fe04889d81cc8d597d503797c0713220e1c53de6
semin <semin@student.42seoul.kr>
2022-11-17 04:27:38 +0900

    fix

--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+	sed -i "s/listen = \/run\/php\/php8.1-fpm.sock/listen = 9000/" "/etc/php/8.1/fpm/pool.d/www.conf";
 
 	# nginx 서버에서 운용 가능
-	chown -R www-data:www-data /var/www/*;
-	chown -R 755 /var/www/*;
+	
 	# mkdir -p /run/php/;
 	# touch /run/php/php8.1-fpm.pid;
 
@@ -25,6 +24,10 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	echo "Wordpress: creating users..."
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author --allow-root;
+
+	chown -R www-data:www-data /var/www/*;
+	chown -R 755 /var/www/*;
+
 	echo "Wordpress: set up!"
 fi
 
