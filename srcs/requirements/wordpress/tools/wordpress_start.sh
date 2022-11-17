c80c9e33b3d451011d79f8f527539e467497afbc
semin <semin@student.42seoul.kr>
2022-11-17 10:29:05 +0900

    wordpress fix

--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
-	sed -i "s/listen = \/run\/php\/php8.1-fpm.sock/listen = 9000/" "/etc/php/8.1/fpm/pool.d/www.conf";
-
-	# nginx 서버에서 운용 가능
+	# sed -i "s/listen = \/run\/php\/php8.1-fpm.sock/listen = 9000/" "/etc/php/8.1/fpm/pool.d/www.conf";
+sed -e
 	
 	# mkdir -p /run/php/;
 	# touch /run/php/php8.1-fpm.pid;
 
-if [ ! -f /var/www/html/wp-config.php ]; then
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	echo "Wordpress: setting up..."
-	mkdir -p /var/www/html
-	# wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
-	# chmod +x wp-cli.phar; 
-	# mv wp-cli.phar /usr/local/bin/wp;
-	cd /var/www/html;
 
-	# 
-	wp core download --allow-root;
+	cp /tmp/wp-config.php /var/www/wordpress/wp-config.php
 
-	wp config create --dbname=wordpress --dbuser=semin --dbpass=1234 --dbhost=mariadb --allow-root
+	# wait for MariaDB
+	sleep 5;
 
-	mv /var/www/wp-config.php /var/www/html/
+	# mkdir -p /var/www/wordpress
+
+	# cd /var/www/wordpress;
+
+	# wp config create --dbname=wordpress --dbuser=semin --dbpass=1234 --dbhost=mariadb --allow-root;
+
+	# mv /var/www/wp-config.php /var/www/wordpress/
 	echo "Wordpress: creating users..."
-	wp core install --allow-root --url=semin.42.fr --title=wordpress --admin_user=eval --admin_password=1234 --admin_email=eval@42.fr
-	wp user create --allow-root semin semin@42.fr --user_pass=1234 --role=author --allow-root;
+	wp core install --allow-root --url=semin.42.fr --title="wordpress" --admin_user=semin --admin_password=1234 --admin_email=semin@42.fr --skip-email
+	wp user create --allow-root wpuser wpuser@42.fr --user_pass=1234 --role=author --allow-root --path=/var/www/wordpress;
 
-	chown -R www-data:www-data /var/www/*;
-	chown -R 755 /var/www/*;
+	# nginx 서버에서 운용 가능
+	# chown -R www-data:www-data /var/www/*;
+	chown -R www-data:www-data /var/www/wordpress/wordpress
+	# chown -R 755 /var/www/*;
 
 	echo "Wordpress: set up!"
 fi
+
+service php8.1-fpm start
+service php8.1-fpm stop
+
+echo "Starting Wordpress Container..."
 
 exec "$@"
