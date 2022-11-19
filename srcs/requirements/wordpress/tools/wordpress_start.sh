1f5ac065dfe8c27e3caa6edf76e7c36717941487
semin <semin@student.42seoul.kr>
2022-11-19 17:42:42 +0900

    change path

--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -6,10 +6,10 @@ sed -e
 	# mkdir -p /run/php/;
 	# touch /run/php/php8.1-fpm.pid;
 
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
+if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 	echo "Wordpress: setting up..."
 
-	cp /tmp/wp-config.php /var/www/wordpress/wp-config.php
+	cp /tmp/wp-config.php /var/www/html/wordpress/wp-config.php
 
 	# wait for MariaDB
 	sleep 5;
@@ -23,11 +23,11 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	# mv /var/www/wp-config.php /var/www/wordpress/
 	echo "Wordpress: creating users..."
 	wp core install --allow-root --url=semin.42.fr --title="wordpress" --admin_user=semin --admin_password=1234 --admin_email=semin@42.fr --skip-email
-	wp user create --allow-root wpuser wpuser@42.fr --user_pass=1234 --role=author --allow-root --path=/var/www/wordpress;
+	wp user create --allow-root wpuser wpuser@42.fr --user_pass=1234 --role=author --allow-root
 
 	# nginx 서버에서 운용 가능
 	# chown -R www-data:www-data /var/www/*;
-	chown -R www-data:www-data /var/www/*
+	chown -R www-data:www-data /var/www/html/wordpress
 	# chown -R 755 /var/www/*;
 
 	echo "Wordpress: set up!"
