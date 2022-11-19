7ed882ed3a3aedbd9154228a5060d1ce6d11acc6
semin <semin@student.42seoul.kr>
2022-11-20 00:08:03 +0900

    using environment variables

--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -4,7 +4,7 @@ if [ ! -f /etc/ssl/certs/nginx.crt ]; then
     echo "Nginx: setting up ssl ...";
 
     # self-signed ssl certification
-    openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=KR/ST=Seoul/L=Seoul/O=wordpress/CN=semin.42.fr";
+    openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout $CERTS_KEY -out $CERTS_ -subj "/C=KR/ST=Seoul/L=Seoul/O=wordpress/CN=$DOMAIN_NAME";
     echo "Nginx: ssl is set up!";
     # cp /tmp/default /etc/nginx/conf.d/default.conf
 
--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,45 +1,3 @@
-# #!/bin/bash
-
-# 	# sed -i "s/listen = \/run\/php\/php8.1-fpm.sock/listen = 9000/" "/etc/php/8.1/fpm/pool.d/www.conf";
-# sed -e
-	
-# 	# mkdir -p /run/php/;
-# 	# touch /run/php/php8.1-fpm.pid;
-
-# if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
-# 	echo "Wordpress: setting up..."
-
-# 	cp /tmp/wp-config.php /var/www/html/wordpress/wp-config.php
-
-# 	# wait for MariaDB
-# 	sleep 5;
-
-# 	# mkdir -p /var/www/wordpress
-
-# 	# cd /var/www/wordpress;
-
-# 	wp config create --dbname=wordpress --dbuser=semin --dbpass=1234 --dbhost=mariadb --allow-root;
-
-# 	# mv /var/www/wp-config.php /var/www/wordpress/
-# 	echo "Wordpress: creating users..."
-# 	wp core install --allow-root --url=semin.42.fr --title="wordpress" --admin_user=semin --admin_password=1234 --admin_email=semin@42.fr --skip-email
-# 	wp user create --allow-root wpuser wpuser@42.fr --user_pass=1234 --role=author --allow-root
-
-# 	# nginx 서버에서 운용 가능
-# 	# chown -R www-data:www-data /var/www/*;
-# 	chown -R www-data:www-data /var/www/html/wordpress
-# 	# chown -R 755 /var/www/*;
-
-# 	echo "Wordpress: set up!"
-# fi
-
-# service php8.1-fpm start
-# service php8.1-fpm stop
-
-# echo "Starting Wordpress Container..."
-
-# exec "$@"
-
 #!/bin/bash
 set -e
 
@@ -50,11 +8,12 @@ if [ ! -f /var/www/html/wordpress/index.php ]; then
 fi
 
 if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
-	wp config create --dbname=wordpress --dbuser=semin --dbpass=1234 --dbhost=mariadb --allow-root
+	wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_ADMIN_LOGIN} --dbpass=${WP_ADMIN_PASSWORD} --dbhost=mariadb --allow-root
 
-	wp core install --url=semin.42.fr --title="semin inception" --admin_user=semin --admin_password=1234 --admin_email=semin@student.42seoul.kr --skip-email --allow-root
-	wp user create wpuser wpuser@student.42seoul.kr --user_pass=1234 --role=author --allow-root
+	wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
+	wp user create ${WP_USER_LOGIN} ${WP_USER_PASSWORD} --user_pass=1234 --role=author --allow-root
 
+	# nginx 서버에서 운용 가능
 	chown -R www-data:www-data /var/www/html/wordpress
 fi
 
